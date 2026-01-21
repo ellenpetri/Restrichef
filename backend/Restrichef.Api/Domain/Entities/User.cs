7db73b1b18f7f3878b4eb634c8d288197f7398e5
Ellen Petri <ellencpetri@gmail.com>
2026-01-21 18:27:45 -0300

    Criação do User.cs e das pastas Domain e Entities

---
+++ backend/Restrichef.Api/Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Restrichef.Api.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Email { get; private set; }
+    public string Senha { get; private set; }
+    public DateTime DataCriacao { get; private set; }
+
+    protected User() { }
+
+    public User(string nome, string email, string senha)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Email = email;
+        Senha = senha;
+        DataCriacao = DateTime.UtcNow;
+    }
+}
