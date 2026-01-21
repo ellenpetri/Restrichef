d4337481fb8883468467ac8417975ad628f7bf23
Ellen Petri <ellencpetri@gmail.com>
2026-01-21 18:42:03 -0300

    Criação da camada Application e caso de uso de cadastro de usuário

---
+++ backend/Restrichef.Api/Application/UseCases/CriarUsuarioUseCase.cs
@@ -0,0 +1,13 @@
+﻿using Restrichef.Api.Domain.Entities;
+
+namespace Restrichef.Api.Application.UseCases;
+
+public class CriarUsuarioUseCase
+{
+    public User Executar(string nome, string email, string senha)
+    {
+        var user = new User(nome, email, senha);
+
+        return user;
+    }
+}
