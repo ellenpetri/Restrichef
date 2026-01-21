77668024460a417b8d5c62c41b52f5db43477714
Ellen Petri <ellencpetri@gmail.com>
2026-01-21 18:15:30 -0300

    Configuração inicial do backend | ASP.NET Core Web API C#

---
+++ backend/Restrichef.Api/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restrichef.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("API running");
+}
