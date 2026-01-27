09c5400d9a2819fc7de0e746ecba99fec9ef7ad1
Ellen Petri <ellencpetri@gmail.com>
2026-01-27 19:33:52 -0300

    Create PerfilAlimentarResponse.cs

---
+++ backend/Restrichef.Api/Controllers/Dtos/PerfilAlimentarResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Restrichef.Api.Controllers.Dtos;
+
+public class PerfilAlimentarResponse
+{
+    public Guid UserId { get; set; }
+    public List<Guid> RestricaoIds { get; set; } = [];
+}
