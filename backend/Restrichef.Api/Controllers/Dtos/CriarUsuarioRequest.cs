7c3898775c7d882cc0aac6fa20e3e0fad29ffaf7
Ellen Petri <ellencpetri@gmail.com>
2026-01-27 19:34:20 -0300

    Update CriarUsuarioRequest.cs

--- backend/Restrichef.Api/Controllers/Dtos/CriarUsuarioRequest.cs
+++ backend/Restrichef.Api/Controllers/Dtos/CriarUsuarioRequest.cs
@@ -2,7 +2,7 @@
 
 public class CriarUsuarioRequest
 {
-    public string Nome { get; set; }
-    public string Email { get; set; }
-    public string Senha { get; set; }
+    public required string Nome { get; set; }
+    public required string Email { get; set; }
+    public required string Senha { get; set; }
 }
