b8d92ddeaab7164eb082c90a8663aa96ca3e073c
Ellen Petri <ellencpetri@gmail.com>
2026-01-28 16:13:40 -0300

    Create JwtTokenService.cs

---
+++ backend/Restrichef.Api/Application/Security/JwtTokenService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Restrichef.Api.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Restrichef.Api.Application.Security;
+
+public class JwtTokenService(IConfiguration configuration)
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public string GerarToken(User user)
+    {
+        IConfigurationSection jwt = _configuration.GetSection("Jwt");
+
+        Claim[] claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email)
+        };
+
+        SymmetricSecurityKey key = new(
+            Encoding.UTF8.GetBytes(jwt["Key"]!)
+        );
+
+        SigningCredentials creds = new(key, SecurityAlgorithms.HmacSha256);
+
+        JwtSecurityToken token = new(
+            issuer: jwt["Issuer"],
+            audience: jwt["Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(
+                int.Parse(jwt["ExpiresInMinutes"]!)
+            ),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
