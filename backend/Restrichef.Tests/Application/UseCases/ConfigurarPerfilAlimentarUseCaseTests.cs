4b096be7d19a89e512764ef63688998310bcd9a4
Ellen Petri <ellencpetri@gmail.com>
2026-01-28 20:12:53 -0300

    Testes Unitários

---
+++ backend/Restrichef.Tests/Application/UseCases/ConfigurarPerfilAlimentarUseCaseTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Restrichef.Api.Application.Repositories;
+using Restrichef.Api.Application.UseCases;
+using Restrichef.Api.Domain.Entities;
+using Restrichef.Api.Infrastructure.Data;
+
+namespace Restrichef.Tests.Application.UseCases;
+
+public class ConfigurarPerfilAlimentarUseCaseTests
+{
+    private static RestrichefDbContext CriarContextoEmMemoria()
+    {
+        DbContextOptions<RestrichefDbContext> options = new DbContextOptionsBuilder<RestrichefDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+        RestrichefDbContext context = new(options);
+        context.Database.EnsureCreated();
+        return context;
+    }
+
+    [Fact]
+    public async Task Deve_substituir_restricoes_quando_perfil_ja_existe()
+    {
+        RestrichefDbContext context = CriarContextoEmMemoria();
+
+        RestricaoAlimentar lactose = context.RestricoesAlimentares.First(r => r.Nome == "Lactose");
+        RestricaoAlimentar gluten = context.RestricoesAlimentares.First(r => r.Nome == "Glúten");
+
+        User user = new("Ellen", "ellen@email.com", "hash");
+        user.DefinirPerfilAlimentar(new PerfilAlimentar(user.Id, [lactose]));
+
+        Mock<IUserRepository> userRepository = new();
+        userRepository.Setup(r => r.GetByIdAsync(user.Id)).ReturnsAsync(user);
+
+        ConfigurarPerfilAlimentarUseCase useCase = new(userRepository.Object, context);
+
+        await useCase.Executar(user.Id, [gluten.Id]);
+
+        user.PerfilAlimentar!.Restricoes.Should().ContainSingle();
+        user.PerfilAlimentar.Restricoes.First().Id.Should().Be(gluten.Id);
+    }
+
+    [Fact]
+    public async Task Deve_lancar_excecao_quando_usuario_nao_existir()
+    {
+        RestrichefDbContext context = CriarContextoEmMemoria();
+
+        Mock<IUserRepository> userRepository = new();
+        userRepository.Setup(r => r.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync((User?)null);
+
+        ConfigurarPerfilAlimentarUseCase useCase = new(userRepository.Object, context);
+
+        Func<Task> act = async () => await useCase.Executar(Guid.NewGuid(), []);
+
+        await act.Should().ThrowAsync<InvalidOperationException>().WithMessage("Usuário não encontrado");
+    }
+
+}
---
+++ backend/Restrichef.Tests/Application/UseCases/FiltrarReceitasPorPerfilUseCaseTests.cs
@@ -0,0 +1,112 @@
+﻿using FluentAssertions;
+using Restrichef.Api.Application.UseCases;
+using Restrichef.Api.Domain.Entities;
+
+namespace Restrichef.Tests.Application.UseCases;
+
+public class FiltrarReceitasPorPerfilUseCaseTests
+{
+    [Fact]
+    public void Deve_retornar_receita_quando_nao_ha_conflito_com_o_perfil()
+    {
+        RestricaoAlimentar lactose = new("Lactose", "Derivados de leite");
+
+        PerfilAlimentar perfil = new(Guid.NewGuid(), [lactose]);
+
+        Ingrediente farinha = new("Farinha", []);
+
+        IngredienteReceita ingredienteReceita = new(farinha, 100, "g");
+
+        Receita receita = new("Pão", "Pão simples", 60, 4);
+
+        receita.Ingredientes.Add(ingredienteReceita);
+
+        List<Receita> receitas = [receita];
+
+        FiltrarReceitasPorPerfilUseCase useCase = new();
+
+        List<Receita> resultado = useCase.Executar(receitas, perfil);
+
+        resultado.Should().ContainSingle();
+        resultado.First().Should().Be(receita);
+    }
+
+    [Fact]
+    public void Deve_remover_receita_quando_ingrediente_conflita_com_o_perfil()
+    {
+        RestricaoAlimentar lactose = new("Lactose", "Derivados de leite");
+
+        PerfilAlimentar perfil = new(Guid.NewGuid(), [lactose]);
+
+        Ingrediente queijo = new("Queijo", [lactose]);
+
+        IngredienteReceita ingredienteReceita = new(queijo, 50, "g");
+
+        Receita receita = new("Pizza", "Pizza com queijo", 30, 2);
+
+        receita.Ingredientes.Add(ingredienteReceita);
+
+        List<Receita> receitas = [receita];
+
+        FiltrarReceitasPorPerfilUseCase useCase = new();
+
+        List<Receita> resultado = useCase.Executar(receitas, perfil);
+
+        resultado.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Deve_remover_receita_quando_qualquer_restricao_do_perfil_conflitar()
+    {
+        RestricaoAlimentar lactose = new("Lactose", "Derivados de leite");
+        RestricaoAlimentar gluten = new("Glúten", "Derivados de trigo");
+
+        PerfilAlimentar perfil = new(Guid.NewGuid(), [lactose, gluten]);
+
+        Ingrediente farinha = new("Farinha de trigo", [gluten]);
+
+        IngredienteReceita ingredienteReceita = new(farinha, 100, "g");
+
+        Receita receita = new("Bolo", "Bolo simples", 45, 6);
+        receita.Ingredientes.Add(ingredienteReceita);
+
+        List<Receita> receitas = [receita];
+
+        FiltrarReceitasPorPerfilUseCase useCase = new();
+
+        List<Receita> resultado = useCase.Executar(receitas, perfil);
+
+        resultado.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Deve_retornar_todas_as_receitas_quando_perfil_nao_possui_restricoes()
+    {
+        PerfilAlimentar perfil = new(Guid.NewGuid(), []);
+
+        RestricaoAlimentar lactose = new("Lactose", "Derivados de leite");
+
+        Ingrediente leite = new("Leite", [lactose]);
+        Ingrediente farinha = new("Farinha", []);
+
+        IngredienteReceita ir1 = new(leite, 200, "ml");
+        IngredienteReceita ir2 = new(farinha, 100, "g");
+
+        Receita receita1 = new("Vitamina", "Vitamina de leite", 5, 1);
+        receita1.Ingredientes.Add(ir1);
+
+        Receita receita2 = new("Pão", "Pão simples", 60, 4);
+        receita2.Ingredientes.Add(ir2);
+
+        List<Receita> receitas = [receita1, receita2];
+
+        FiltrarReceitasPorPerfilUseCase useCase = new();
+
+        List<Receita> resultado = useCase.Executar(receitas, perfil);
+
+        resultado.Should().HaveCount(2);
+        resultado.Should().Contain(recipes => recipes == receita1);
+        resultado.Should().Contain(recipes => recipes == receita2);
+    }
+
+}
