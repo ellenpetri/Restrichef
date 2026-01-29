2ac414375c5431c9f224b445b3ab4fa744ed4f4a
Ellen Petri <ellencpetri@gmail.com>
2026-01-29 19:29:47 -0300

    Create colors.ts

---
+++ frontend/restrichef-frontend/src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  primary: "#16B981",
+  background: "#F6FBFF",
+  card: "#FFFFFF",
+  inputBg: "#EAF2FF",
+  inputBorder: "#C7DAFF",
+  textTitle: "#0B1F44",
+  textBody: "#3B5B8A",
+};
